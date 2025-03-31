1472aec5ee21f68c79aec337440f95204489835f
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2025-03-30 23:44:04 -0300

    Agregar middleware de validación para superhéroes y reglas de validación en las rutas

---
+++ src/routes/errorMiddleware/superHeroeErrorMiddleware.mjs
@@ -0,0 +1,18 @@
+import { validationResult } from "express-validator";
+
+export const handleValidationSuperHeroeErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+
+        console.log(errors.array());
+        return res.status(400).json({ 
+            status:'error',
+            message: 'Error de validación',
+            errors: errors.array().map(error => ({
+                field: error.path,
+                message: error.msg
+            }))
+         });
+    }
+    next();
+}
--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -8,6 +8,8 @@ import { obtenerSuperHeroePorIdController,
     eliminarSuperHeroePorIdController,
     eliminarSuperHeroePorNombreSuperHeroeController
 } from '../controllers/superheroesController.mjs';
+import { handleValidationSuperHeroeErrors } from './errorMiddleware/superHeroeErrorMiddleware.mjs';
+import { registerSuperHeroValidationRules } from './validations/superHerovalidationRules.mjs';
 
 const  router = express.Router();
 
@@ -15,7 +17,7 @@ router.get('/heroes', obtenerTodosLosSuperHeroesController);
 router.get('/heroes/mayores-30', obtenerSuperHeroesMayoresDe30Controller);
 router.get('/heroes/:id', obtenerSuperHeroePorIdController);
 router.get('/heroes/buscar/:atributo/:value', buscarSuperHeroesPorAtributoController);
-router.post('/heroes', agregarSuperHeroeController);
+router.post('/heroes',registerSuperHeroValidationRules(), handleValidationSuperHeroeErrors ,agregarSuperHeroeController);
 router.put('/heroes/:id', editarSuperHeroeController);
 router.delete('/heroes/:id', eliminarSuperHeroePorIdController);
 router.delete('/heroes/nombre/:nombreSuperHeroe', eliminarSuperHeroePorNombreSuperHeroeController);
